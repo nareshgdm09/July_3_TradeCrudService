378962786d7705016fd4b5a6b45b3c06f25dd3a1
nareshgdm09 <nareshg4020@gmail.com>
2019-07-04 10:42:03 +0530

    adding more test cases with service test

--- TradeCRUDService/src/test/java/com/tradetask/controller/TradeSearchRestControllerTest.java
+++ TradeCRUDService/src/test/java/com/tradetask/controller/TradeSearchRestControllerTest.java
@@ -1,7 +1,11 @@
 package com.tradetask.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,11 +21,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tradetask.entity.Trade;
 import com.tradetask.service.Impl.TradeServiceImpl;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TradeSearchRestControllerTest {
-	private MockMvc mvc;
+	private MockMvc mockMvc;
 
 	@Mock
 	private TradeServiceImpl tradeServiceImpl;
@@ -33,12 +38,12 @@ public class TradeSearchRestControllerTest {
 	public void setup() {
 
 		JacksonTester.initFields(this, new ObjectMapper());
-		mvc = MockMvcBuilders.standaloneSetup(tradeSearchRestController).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(tradeSearchRestController).build();
 	}
 
 	@Test
 	public void testFindByTradeIdWhenNotFound() throws Exception {
-		MockHttpServletResponse response = mvc
+		MockHttpServletResponse response = mockMvc
 				.perform(get("/v2/trades/{tradeId}", 1).accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
 		System.out.println("response in /v2/trades/{tradeId} " + response.toString());
 		assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
@@ -46,9 +51,19 @@ public class TradeSearchRestControllerTest {
 
 	@Test
 	public void testFindByTradeIdWhenFound() throws Exception {
-		MockHttpServletResponse response = mvc.perform(get("/v2/test")).andReturn().getResponse();
+		MockHttpServletResponse response = mockMvc.perform(get("/v2/test")).andReturn().getResponse();
 		System.out.println("Returned Response " + response.getStatus());
 		assertThat(response.getStatus()).isEqualTo(HttpStatus.FOUND.value());
+	}
+
+	@Test
+	public void testFindByTradeIdWhenFound1() throws Exception {
+		
+		when(tradeServiceImpl.findById(123)).thenReturn(new Trade());
+
+		mockMvc.perform(get("/v2/trades/{tradeId}", 123)).andExpect(status().isFound());
+
+		verify(tradeServiceImpl).findById(123);
 	}
 
 }
